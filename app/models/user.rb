1ed997ad8f1002d9bc3ca1cad1a4992ce567e263
s-keito <110315888+s-keito@users.noreply.github.com>
2022-08-13 11:37:03 +0000

    [add2]task3

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,15 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
 has_many :books, dependent: :destroy
-has_one_attached :image
+has_one_attached :user_image
+
+def get_user_image
+  unless user_image.attached?
+    file_path = Rails.root.join('app/assets/images/no_image.jpg')
+    user_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+  end
+    user_image
+end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,12 @@
 Rails.application.routes.draw do
 
-  
-  get 'users/index'
-  get 'users/show'
-  get 'users/edit'
+
+
   devise_for :users
   root to: 'homes#top'
-  
-  resources :books, only: [:new, :index, :show]
-  
+
+  resources :books, only: [:new, :index, :show, :edit, :create]
+  resources :users, only: [:edit, :index, :show, :update]
   get 'homes/about' => 'homes#about', as: 'about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
